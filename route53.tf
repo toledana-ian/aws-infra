0de67905f647c2fd723e96fdb5fd95c2ed349542
Christian Toledana <toledana.ian@gmail.com>
2024-01-10 05:54:33 +0800

    Refactor route53 records and add Sendgrid related records
    Renamed the module "route53_records" to "route_53_github_records" and refactored its record set. Also, a new module "route_53_sendgrid_records" has been introduced to handle Sendgrid related DNS records. These changes will help in better grouping and managing the record sets in AWS Route53.

--- route53.tf
+++ route53.tf
@@ -11,24 +11,13 @@ module "route_53_zone" {
   }
 }
 
-module "route_53_records" {
+module "route_53_github_records" {
   source  = "terraform-aws-modules/route53/aws//modules/records"
   version = "~> 2.0"
 
   zone_name = keys(module.route_53_zone.route53_zone_zone_id)[0]
 
   records = [
-#    {
-#      name    = "christiantoledana.com"
-#      type    = "A"
-#      ttl     = 300
-#      records = [
-#        "185.199.108.153",
-#        "185.199.109.153",
-#        "185.199.110.153",
-#        "185.199.111.153",
-#      ]
-#    },
     {
       name    = "www"
       type    = "A"
@@ -41,11 +30,78 @@ module "route_53_records" {
       ]
     },
     {
+      name  = "christiantoledana.com"
+      type  = "A"
+      alias = {
+        name = "www.christiantoledana.com"
+      }
+    },
+    {
       name    = "_github-pages-challenge-toledana-ian"
       type    = "TXT"
       ttl     = 300
       records = [
         "813f9406ab172f340161ed3607c926",
+      ]
+    },
+  ]
+
+  depends_on = [module.route_53_zone]
+}
+
+module "route_53_sendgrid_records" {
+  source  = "terraform-aws-modules/route53/aws//modules/records"
+  version = "~> 2.0"
+
+  zone_name = keys(module.route_53_zone.route53_zone_zone_id)[0]
+
+  records = [
+    {
+      name    = "em9467"
+      type    = "CNAME"
+      ttl     = 300
+      records = [
+        "u26320669.wl170.sendgrid.net",
+      ]
+    },
+    {
+      name    = "s1._domainkey"
+      type    = "CNAME"
+      ttl     = 300
+      records = [
+        "s1.domainkey.u26320669.wl170.sendgrid.net",
+      ]
+    },
+    {
+      name    = "s2._domainkey"
+      type    = "CNAME"
+      ttl     = 300
+      records = [
+        "s2.domainkey.u26320669.wl170.sendgrid.net",
+      ]
+    },
+    {
+      name    = "url7048"
+      type    = "CNAME"
+      ttl     = 300
+      records = [
+        "sendgrid.net",
+      ]
+    },
+    {
+      name    = "26320669"
+      type    = "CNAME"
+      ttl     = 300
+      records = [
+        "sendgrid.net",
+      ]
+    },
+    {
+      name    = "_dmarc"
+      type    = "TXT"
+      ttl     = 300
+      records = [
+        "v=DMARC1; p=none;",
       ]
     },
   ]
