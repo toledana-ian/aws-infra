9d40cdb16de6b04bbcec038b280fe9ec2021fa35
Christian Toledana <toledana.ian@gmail.com>
2024-01-11 17:51:01 +0800

    Enable ACM certificate and update DNS records
    ACM certificate resources have been enabled in the 'acm_certificate.tf' file. Furthermore, the root and www DNS records have been updated in the 'route53_zone_records_github.tf' file to reflect these changes. These changes ensure both secure certificate validation for the domain and accurate DNS resolution across the records.

--- acm_certificate.tf
+++ acm_certificate.tf
@@ -1,16 +1,12 @@
-#resource "aws_acm_certificate" "christiantoledana_com" {
-#  domain_name       = aws_route53_zone.christiantoledana_com.name
-#  validation_method = "DNS"
-#  tags              = var.default_tags
-#
-#  lifecycle {
-#    create_before_destroy = true
-#  }
-#}
-#
-#resource "aws_acm_certificate_validation" "christiantoledana_com" {
-#  certificate_arn = aws_acm_certificate.christiantoledana_com.arn
-#  validation_record_fqdns = [
-#    for record in aws_acm_certificate.christiantoledana_com.domain_validation_options : record.resource_record_name
-#  ]
-#}
+resource "aws_acm_certificate" "christiantoledana_com" {
+  domain_name       = aws_route53_zone.christiantoledana_com.name
+  validation_method = "DNS"
+  tags              = var.default_tags
+}
+
+resource "aws_acm_certificate_validation" "christiantoledana_com" {
+  certificate_arn = aws_acm_certificate.christiantoledana_com.arn
+  validation_record_fqdns = [
+    "${aws_route53_record.christiantoledana_com_certificate_validation.fqdn}"
+  ]
+}
--- route53_zone_records_github.tf
+++ route53_zone_records_github.tf
@@ -2,6 +2,8 @@ resource "aws_route53_record" "www_christiantoledana_com" {
   name    = "www"
   type    = "A"
   zone_id = aws_route53_zone.christiantoledana_com.id
+  ttl     = 300
+
   records = [
     "185.199.108.153",
     "185.199.109.153",
@@ -14,9 +16,21 @@ resource "aws_route53_record" "christiantoledana_com" {
   name    = ""
   type    = "A"
   zone_id = aws_route53_zone.christiantoledana_com.id
+
   alias {
-    name                   = aws_route53_record.www_christiantoledana_com.name
-    zone_id                = aws_route53_record.www_christiantoledana_com.id
+    name                   = "${aws_route53_record.www_christiantoledana_com.name}.${aws_route53_zone.christiantoledana_com.name}"
+    zone_id                = aws_route53_zone.christiantoledana_com.id
     evaluate_target_health = false
   }
+}
+
+resource "aws_route53_record" "christiantoledana_com_certificate_validation" {
+  name    = "${tolist(aws_acm_certificate.christiantoledana_com.domain_validation_options)[0].resource_record_name}"
+  type    = "${tolist(aws_acm_certificate.christiantoledana_com.domain_validation_options)[0].resource_record_type}"
+  zone_id = aws_route53_zone.christiantoledana_com.id
+  ttl     = "60"
+
+  records = [
+    "${tolist(aws_acm_certificate.christiantoledana_com.domain_validation_options)[0].resource_record_value}"
+  ]
 }
