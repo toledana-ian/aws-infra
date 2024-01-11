51ce787ab12fdd487930d64afbf158054ab77b6f
Christian Toledana <toledana.ian@gmail.com>
2024-01-11 19:41:46 +0800

    Update AWS Terraform provider and add S3 backend configuration
    Added versioning to AWS provider and rearranged AWS provider source in the terraform configuration file. Also, implemented backend configuration for S3 to provide more control over state storage and prevent manual changes on the infrastructure. This will also

--- main.tf
+++ main.tf
@@ -1,11 +1,20 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
+      source = "hashicorp/aws"
+      version = "~> 5.31.0"
     }
+  }
+
+  backend "s3" {
+    bucket         = "christiantoledana-terraform-state"
+    key            = "state/terraform.tfstate"
+    region         = "ap-southeast-1"
+    encrypt        = true
+    dynamodb_table = "terraform-state-lockid"
   }
 }
 
 provider "aws" {
-  region  = "ap-southeast-1"
+  region = "ap-southeast-1"
 }
