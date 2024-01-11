f630712224cd92edb4fa0b782dd532b28c2aa774
Christian Toledana <toledana.ian@gmail.com>
2024-01-11 20:39:14 +0800

    Removed domain and email variables from Terraform configuration
    The variables 'domain_name' and 'default_email' have been removed from the 'variables.tf' Terraform configuration file. This change is part of a larger effort to streamline and simplify setup, reducing the number of user-defined variables for better maintainability.

--- variables.tf
+++ variables.tf
@@ -1,13 +1,3 @@
-variable "domain_name" {
-  description = "The main domain of this config"
-  default     = "christiantoledana.com"
-}
-
-variable "default_email" {
-  description = "The main email of this config"
-  default     = "admin@christiantoledana.com"
-}
-
 variable "default_tags" {
   description = "The default tags of resources"
   default = {
