a521f71fb89c7e00b3d4408ec60323363f91e1c1
Christian Toledana <toledana.ian@gmail.com>
2024-01-13 15:04:43 +0800

    Refactor domain name routing in Terraform
    This commit refactors the modules.tf and output.tf files in the Terraform production and global directories, respectively. Changes include extracting the routing domain name as a variable from the remote state output instead of hardcoding it, and adding an output for the domain name in the global output.tf file. This enhances configuration flexibility and maintainability.

--- global/output.tf
+++ global/output.tf
@@ -6,6 +6,10 @@ output "acm_certificate_arn_christiantoledana_com" {
   value = aws_acm_certificate.christiantoledana_com.arn
 }
 
+output "route53_zone_name_christiantoledana_com" {
+  value = aws_route53_zone.christiantoledana_com.name
+}
+
 output "route53_zone_id_christiantoledana_com" {
   value = aws_route53_zone.christiantoledana_com.id
 }
--- production/modules.tf
+++ production/modules.tf
@@ -2,8 +2,8 @@ module "email-blast-composer" {
   source = "../modules/email-blast-composer"
   name   = "prod-email-blast-composer"
 
-  route_domain_name = "christiantoledana.com"
   route_sub_domain_name = "dynamdev-email-blast-composer"
+  route_domain_name = data.terraform_remote_state.global.outputs.route53_zone_name_christiantoledana_com
   route_zone_id = data.terraform_remote_state.global.outputs.route53_zone_id_christiantoledana_com
 
   acm_certificate_arn = data.terraform_remote_state.global.outputs.acm_certificate_arn_christiantoledana_com
