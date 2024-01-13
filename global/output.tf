654fe76ee0fa2383b2555480b4ac80e3f1dffed9
Christian Toledana <toledana.ian@gmail.com>
2024-01-13 15:11:17 +0800

    Remove hardcoded domain routing in Terraform
    The 'output.tf' in both 'production' and 'global' directories was cleaned up by removing hard-coded domain name servers concerning 'christiantoledana_com'. This change promotes flexibility and eases future configuration adjustments.

--- global/output.tf
+++ global/output.tf
@@ -1,7 +1,3 @@
-output "route53_zone_name_servers_christiantoledana_com" {
-  value = aws_route53_zone.christiantoledana_com.name_servers
-}
-
 output "acm_certificate_arn_christiantoledana_com" {
   value = aws_acm_certificate.christiantoledana_com.arn
 }
--- production/output.tf
+++
@@ -1,3 +0,0 @@
-output "route53_zone_name_servers_christiantoledana_com" {
-  value = data.terraform_remote_state.global.outputs.route53_zone_name_servers_christiantoledana_com
-}
