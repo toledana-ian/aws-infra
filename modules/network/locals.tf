7fa928fadc894868f2202cfdfcc2768be3f86c8e
Christian Toledana <toledana.ian@gmail.com>
2024-01-14 14:41:32 +0800

    Update subnet CIDR blocks in network module
    The CIDR blocks for both public and private subnets in all environments (production, staging, testing, development) have been updated from "/16" to "/24". This change reduces the total number of available IP addresses in each subnet, thereby optimizing IP address usage and management.

--- modules/network/locals.tf
+++ modules/network/locals.tf
@@ -7,16 +7,16 @@ locals {
   }
 
   subnet_public_cidr_block = {
-    production  = "10.0.0.0/16"
-    staging     = "10.1.0.0/16"
-    testing     = "10.2.0.0/16"
-    development = "10.3.0.0/16"
+    production  = "10.0.0.0/24"
+    staging     = "10.1.0.0/24"
+    testing     = "10.2.0.0/24"
+    development = "10.3.0.0/24"
   }
 
   subnet_private_cidr_block = {
-    production  = "10.0.1.0/16"
-    staging     = "10.1.1.0/16"
-    testing     = "10.2.1.0/16"
-    development = "10.3.1.0/16"
+    production  = "10.0.1.0/24"
+    staging     = "10.1.1.0/24"
+    testing     = "10.2.1.0/24"
+    development = "10.3.1.0/24"
   }
 }
