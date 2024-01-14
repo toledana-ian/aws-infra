081ab7a94fb6ce7a8c13d84448f6119b3e751e4d
Christian Toledana <toledana.ian@gmail.com>
2024-01-14 23:24:44 +0800

    Update S3 bucket files handling scripts
    Added comments and functionalities in `list_s3_zip_contents.sh` and `get_s3_zip_code_hash.sh` scripts. These provide clearer instructions and improve the way we handle the files in an S3 bucket. Differences include better JSON conversion for filenames, more efficient temporary storage handling, and improved ETag fetching.

--- modules/app-email-blast-composer/scripts/get_s3_zip_code_hash.sh
+++ modules/app-email-blast-composer/scripts/get_s3_zip_code_hash.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-# Check if two arguments are provided
+# This script fetches the ETag (MD5 hash) of a file present in an S3 bucket
+# and prints it in a JSON format.
+
+# It expects two arguments: the S3 bucket name and the key of the file
+# present in that bucket.
+
+# Check if two arguments are provided.
+# If not, print the usage and exit.
 if [ "$#" -ne 2 ]; then
     echo "Usage: $0 <BUCKET_NAME> <ZIP_FILE_KEY>"
     exit 1
@@ -9,10 +16,16 @@ fi
 BUCKET_NAME="$1"
 ZIP_FILE_KEY="$2"
 
-# Get the ETag (MD5 hash) of the uploaded zip file in S3
+# The `aws s3api head-object` command fetches the metadata of an object
+# in S3. The `--query 'ETag'` option is used to retrieve only the ETag
+# of the object.
+# The ETag of an S3 object is the MD5 hash of the object and is wrapped
+# in double quotes. We save this to the `etag` variable
 etag=$(aws s3api head-object --bucket $BUCKET_NAME --key $ZIP_FILE_KEY --query 'ETag' --output text)
 
-# Calculate the source_code_hash
+# We then remove the double quotes from `etag` and save this to the
+# `source_code_hash` variable.
 source_code_hash=$(echo -n $etag | tr -d '"')
 
+# Finally we print the source code hash in a JSON format.
 echo "{\"source_code_hash\":\"$source_code_hash\"}"
--- modules/app-email-blast-composer/scripts/list_s3_zip_contents.sh
+++ modules/app-email-blast-composer/scripts/list_s3_zip_contents.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# This script is used to download a zip file from an S3 bucket, list its contents and convert the filenames into a JSON object.
+
 # Check if two arguments are provided
 if [ "$#" -ne 2 ]; then
     echo "Usage: $0 <BUCKET_NAME> <ZIP_FILE_KEY>"
@@ -9,12 +11,16 @@ fi
 BUCKET_NAME="$1"
 ZIP_FILE_KEY="$2"
 
+# Function to convert filenames into a JSON object.
+# It receives the filenames one by one from the while loop and constructs a JSON object.
+# If the filename is not empty, it removes the file extension and adds an entry into the JSON object.
 function filenames_to_json {
     local json="{"
     local first=true
     while IFS= read -r line; do
         if [[ $line =~ ^[[:space:]]+[0-9]+ ]]; then
             local filename
+
             filename=$(echo "$line" | awk '{print $4}')
             if [ -n "$filename" ]; then
                 # Remove file extension using parameter expansion
@@ -32,7 +38,14 @@ function filenames_to_json {
     echo "$json"
 }
 
+# Temporarily stores the downloaded zip file
 TMP_ZIP_FILE=$(mktemp)
+
+# Download the zip file from the S3 bucket and store it in the temporary location
 aws s3 cp "s3://${BUCKET_NAME}/${ZIP_FILE_KEY}" "${TMP_ZIP_FILE}" --quiet
+
+# List the contents of the zip file and convert the filenames into a JSON object
 unzip -l "${TMP_ZIP_FILE}" | filenames_to_json
+
+# Remove the temporary zip file
 rm "${TMP_ZIP_FILE}"
