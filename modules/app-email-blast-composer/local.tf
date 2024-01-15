3c6863590d7503a15ca6bc7b13b72674667a0659
Christian Toledana <toledana.ian@gmail.com>
2024-01-16 02:15:40 +0800

    Refactor local variables in `app-email-blast-composer` module
    Included additional variables such as lambda complex functions and simple rest functions in `locals` block of `app-email-blast-composer` Terraform module. Also added descriptive comments for understanding and maintenance ease. Besides, the API Gateway domain has been added to this section.

--- modules/app-email-blast-composer/local.tf
+++ modules/app-email-blast-composer/local.tf
@@ -1,9 +1,16 @@
 locals {
+  // name of the file where the lambdas are stored
   lambda_zip_filename = "lambdas.zip"
 
-  lambda_functions = keys(data.external.get_lambda_list.result)
-
+  // hash of the source code for the lambda
   lambda_source_code_hash =  data.external.get_lambda_source_code_hash.result["source_code_hash"]
 
+  // list of complex lambda functions
+  lambda_complex_functions = ['send-email', 'queue-email']
+
+  // list of simple rest lambda functions obtained by excluding the complex ones from the full set
+  lambda_simple_rest_functions = setsubtract(keys(data.external.get_lambda_list.result), lambda_complex_functions)
+
+  // the domain of the api gateway
   api_gateway_domain = split("/", aws_api_gateway_deployment.api.invoke_url)[2]
 }
