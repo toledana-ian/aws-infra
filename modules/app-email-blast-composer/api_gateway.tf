781049bd8121ccbec66301a8bc00c17dd623a24d
Christian Toledana <toledana.ian@gmail.com>
2024-01-16 06:25:25 +0800

    Refactor AWS API Gateway setup in email-blast-composer module
    The AWS API Gateway setup has been refactored in the 'app-email-blast-composer' module. The 'aws_api_gateway_resource' and 'aws_api_gateway_integration' resources have been renamed to 'simple_rest_api', with corresponding changes to their method and integration. An explicit 'aws_api_gateway_deployment' resource has also been added at the top.

--- modules/app-email-blast-composer/api_gateway.tf
+++ modules/app-email-blast-composer/api_gateway.tf
@@ -5,7 +5,15 @@ resource "aws_api_gateway_rest_api" "api" {
   tags = var.tags
 }
 
-resource "aws_api_gateway_resource" "api" {
+resource "aws_api_gateway_deployment" "api" {
+  depends_on = [
+    aws_api_gateway_integration.simple_rest_api
+  ]
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  stage_name  = "default"
+}
+
+resource "aws_api_gateway_resource" "simple_rest_api" {
   for_each = toset(local.lambda_simple_rest_functions)
 
   rest_api_id = aws_api_gateway_rest_api.api.id
@@ -13,30 +21,22 @@ resource "aws_api_gateway_resource" "api" {
   path_part   = each.value
 }
 
-resource "aws_api_gateway_method" "api" {
+resource "aws_api_gateway_method" "simple_rest_api" {
   for_each = toset(local.lambda_simple_rest_functions)
 
   rest_api_id   = aws_api_gateway_rest_api.api.id
-  resource_id   = aws_api_gateway_resource.api[each.value].id
+  resource_id   = aws_api_gateway_resource.simple_rest_api[each.value].id
   http_method   = "POST"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "api" {
+resource "aws_api_gateway_integration" "simple_rest_api" {
   for_each = toset(local.lambda_simple_rest_functions)
 
   rest_api_id             = aws_api_gateway_rest_api.api.id
-  resource_id             = aws_api_gateway_resource.api[each.value].id
-  http_method             = aws_api_gateway_method.api[each.value].http_method
+  resource_id             = aws_api_gateway_resource.simple_rest_api[each.value].id
+  http_method             = aws_api_gateway_method.simple_rest_api[each.value].http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.api[each.value].invoke_arn
-}
-
-resource "aws_api_gateway_deployment" "api" {
-  depends_on = [
-    aws_api_gateway_integration.api
-  ]
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  stage_name  = "default"
 }
