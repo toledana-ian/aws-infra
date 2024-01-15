1bde050a7a28c23fac2b0fd3d185ca08a95daba1
Christian Toledana <toledana.ian@gmail.com>
2024-01-16 06:05:43 +0800

    Reduce CloudWatch log retention and modify IAM role
    Reduced the CloudWatch log retention period from 30 to 7 days in 'app-email-blast-composer' module. Also, renamed the IAM role from 'lambda' to 'api' and added a new IAM role policy. These changes help in efficient log management and streamlining permission control for different AWS services.

--- modules/app-email-blast-composer/cloudwatch.tf
+++ modules/app-email-blast-composer/cloudwatch.tf
@@ -2,7 +2,8 @@ resource "aws_cloudwatch_log_group" "api" {
   for_each = toset(local.lambda_simple_rest_functions)
 
   name              = "/aws/lambda/${aws_lambda_function.api[each.value].function_name}"
-  retention_in_days = 30
+  retention_in_days = 7
 
   tags = var.tags
 }
+
--- modules/app-email-blast-composer/iam.tf
+++ modules/app-email-blast-composer/iam.tf
@@ -1,17 +1,29 @@
-resource "aws_iam_role" "lambda" {
-  name = "${var.name}-serverless-lambda"
+resource "aws_iam_role" "api" {
+  name = "${var.name}-lambda"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
+    Version   = "2012-10-17"
     Statement = [
       {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
+        Action    = "sts:AssumeRole"
+        Effect    = "Allow"
+        Sid       = ""
         Principal = {
           Service = "lambda.amazonaws.com"
         }
       }
     ]
   })
+}
+
+resource "aws_iam_role_policy" "api" {
+  role   = aws_iam_role.api.id
+  policy = data.aws_iam_policy_document.api.json
+}
+
+data "aws_iam_policy_document" "api" {
+  statement {
+    actions   = ["logs:CreateLogGroup", "logs:CreateLogStream", "logs:PutLogEvents"]
+    resources = ["arn:aws:logs:*:*:*"]
+  }
 }
