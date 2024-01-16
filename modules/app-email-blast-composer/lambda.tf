5a86928abc0cae287b5215753cddcea16ae0cb00
Christian Toledana <toledana.ian@gmail.com>
2024-01-17 07:31:11 +0800

    Update environment variables in lambda.tf
    Reformatted environment variables related to Sendgrid secrets in the lambda.tf file. Removed SENDGRID_SECRET_NAME from one module and added it to another for better secret handling and management.

--- modules/app-email-blast-composer/lambda.tf
+++ modules/app-email-blast-composer/lambda.tf
@@ -42,7 +42,6 @@ resource "aws_lambda_function" "queue_email" {
   environment {
     variables = {
       QUEUE_URL            = aws_sqs_queue.email.url
-      SENDGRID_SECRET_NAME = aws_secretsmanager_secret.sendgrid.name
     }
   }
 
@@ -87,7 +86,7 @@ resource "aws_lambda_function" "send_email" {
 
   environment {
     variables = {
-      SENDGRID_API_KEY = aws_secretsmanager_secret.sendgrid.arn
+      SENDGRID_SECRET_NAME = aws_secretsmanager_secret.sendgrid.name
     }
   }
 
