65fab15c1a840a3b2d3e5c65d43fb8800033f343
Christian Toledana <toledana.ian@gmail.com>
2024-01-22 05:45:50 +0800

    Add authentication subdomain for AWS Cognito
    Introduced new auth subdomain specifically tailored for the AWS Cognito within the email-blast-composer module. This modification includes adding a new variable for the subdomain and adapting the AWS Cognito user pool and user pool client accordingly. The related permissions and constraints of the client are not changed.

--- production/modules.tf
+++ production/modules.tf
@@ -2,10 +2,11 @@ module "app-email-blast-composer" {
   source = "../modules/app-email-blast-composer"
   name   = "prod-email-blast-composer"
 
-  route_app_sub_domain_name   = "dynamdev-email-blast-composer"
-  route_api_sub_domain_name   = "dynamdev-email-blast-composer-api"
-  route_domain_name           = data.terraform_remote_state.global.outputs.route53_zone_name_christiantoledana_com
-  route_zone_id               = data.terraform_remote_state.global.outputs.route53_zone_id_christiantoledana_com
+  route_app_sub_domain_name  = "dynamdev-email-blast-composer"
+  route_api_sub_domain_name  = "dynamdev-email-blast-composer-api"
+  route_auth_sub_domain_name = "dynamdev-email-blast-composer-auth"
+  route_domain_name          = data.terraform_remote_state.global.outputs.route53_zone_name_christiantoledana_com
+  route_zone_id              = data.terraform_remote_state.global.outputs.route53_zone_id_christiantoledana_com
 
   acm_certificate_arn = data.terraform_remote_state.global.outputs.acm_certificate_arn_christiantoledana_com
 
