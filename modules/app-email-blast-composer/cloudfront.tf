128f98c4dfb9f554ca8e6621c36932520a70b350
Christian Toledana <toledana.ian@gmail.com>
2024-04-21 21:07:50 +0800

    Add basic auth to CloudFront distribution
    The `cloudfront.tf` file in the app-email-blast-composer module now includes a new `aws_cloudfront_function` for basic authentication. This function checks the incoming request for the required Authorization header and returns an appropriate challenge if it's missing or incorrect. It's associated with the viewer-request event of the CloudFront distribution to enhance its security.

--- modules/app-email-blast-composer/cloudfront.tf
+++ modules/app-email-blast-composer/cloudfront.tf
@@ -2,6 +2,41 @@ resource "aws_cloudfront_origin_access_identity" "app" {
   comment = "origin-access-identity-${var.name}"
 }
 
+resource "aws_cloudfront_function" "basic_auth" {
+  name    = "${var.name}-basic-auth"
+  runtime = "cloudfront-js-2.0"
+  code    = <<-EOF
+    function handler(event) {
+      var authHeaders = event.request.headers.authorization;
+
+      // The Base64-encoded Auth string that should be present.
+      // It is an encoding of `Basic base64([username]:[password])`
+      var expected = "Basic ZHluYW06JiFlJTNONWRkJHgza150OQ==";
+
+      // If an Authorization header is supplied and it's an exact match, pass the
+      // request on through to CF/the origin without any modification.
+      if (authHeaders && authHeaders.value === expected) {
+        return event.request;
+      }
+
+      // But if we get here, we must either be missing the auth header or the
+      // credentials failed to match what we expected.
+      // Request the browser present the Basic Auth dialog.
+      var response = {
+        statusCode: 401,
+        statusDescription: "Unauthorized",
+        headers: {
+          "www-authenticate": {
+            value: 'Basic realm="Enter credentials for this super secure site"',
+          },
+        },
+      };
+
+      return response;
+    }
+  EOF
+}
+
 resource "aws_cloudfront_distribution" "app" {
   aliases = [
     var.route_app_sub_domain_name == "" ? var.route_domain_name : format("%s.%s", var.route_app_sub_domain_name, var.route_domain_name)
@@ -46,6 +81,11 @@ resource "aws_cloudfront_distribution" "app" {
       cookies {
         forward = "none"
       }
+    }
+
+    function_association {
+      event_type   = "viewer-request"
+      function_arn = aws_cloudfront_function.basic_auth.arn
     }
 
     viewer_protocol_policy = "redirect-to-https"
