b7421f454f18377dc11e8b7cfc1caf808b589135
Christian Toledana <toledana.ian@gmail.com>
2024-04-22 01:57:24 +0800

    Refactor authorization logic in email blast composer's Lambda function
    The basic authorization logic in the AWS Lambda function of the email blast composer module has been simplified and refactored. The code now checks headers directly in the request object and consistently responds with an error message if the headers are absent or incorrect. There is also an added response for valid authorizations.

--- modules/app-email-blast-composer/data.tf
+++ modules/app-email-blast-composer/data.tf
@@ -13,22 +13,7 @@ data "archive_file" "lambda_cloudfront_basic_auth_source_code"{
     filename = "index.js"
     content  = <<-EOF
       function handler(event) {
-        var authHeaders = event.request.headers.authorization;
-
-        // The Base64-encoded Auth string that should be present.
-        // It is an encoding of `Basic base64([username]:[password])`
-        var expected = "Basic ZHluYW06JiFlJTNONWRkJHgza150OQ==";
-
-        // If an Authorization header is supplied and it's an exact match, pass the
-        // request on through to CF/the origin without any modification.
-        if (authHeaders && authHeaders.value === expected) {
-          return event.request;
-        }
-
-        // But if we get here, we must either be missing the auth header or the
-        // credentials failed to match what we expected.
-        // Request the browser present the Basic Auth dialog.
-        var response = {
+        var errorResponse = {
           statusCode: 401,
           statusDescription: "Unauthorized",
           headers: {
@@ -38,7 +23,24 @@ data "archive_file" "lambda_cloudfront_basic_auth_source_code"{
           },
         };
 
-        return response;
+        var request = event.request;
+
+        if (!request || !request.headers) {
+          return errorResponse;
+        }
+
+        var authHeaders = request.headers.authorization;
+
+        if (!authHeaders || authHeaders.value !== "Basic ZHluYW06JiFlJTNONWRkJHgza150OQ==") {
+          return errorResponse;
+        }
+
+        return {
+          statusCode: 200,
+          statusDescription: 'OK',
+          headers: request.headers,
+          body: request.body,
+        };
       }
 
       module.exports.handler = handler;
