f18b5718772c5d8a562b6ee2c2815dbcf0a4672b
Christian Toledana <toledana.ian@gmail.com>
2024-04-22 22:54:46 +0800

    Refactor function and remove unnecessary identifier
    The authorization handling inside the Lambda function in the email blast composer has been refactored to directly inspect the request headers, providing a more streamlined approach. All extraneous parameters and identifiers, including an unnecessary Sid in iam.tf, have been removed. As part of this commit, an error message is returned for missing or incorrect headers, and a status response is provided for successful authorization.

--- modules/app-email-blast-composer/data.tf
+++ modules/app-email-blast-composer/data.tf
@@ -1,46 +1,40 @@
 data "external" "get_lambda_list" {
-  program = ["bash", "${path.module}/scripts/list_s3_zip_contents.sh", aws_s3_bucket.app.bucket, local.lambda_zip_filename]
+  program = [
+    "bash", "${path.module}/scripts/list_s3_zip_contents.sh", aws_s3_bucket.app.bucket, local.lambda_zip_filename
+  ]
 }
 
 data "external" "get_lambda_source_code_hash" {
-  program = ["bash", "${path.module}/scripts/get_s3_zip_code_hash.sh", aws_s3_bucket.app.bucket, local.lambda_zip_filename]
+  program = [
+    "bash", "${path.module}/scripts/get_s3_zip_code_hash.sh", aws_s3_bucket.app.bucket, local.lambda_zip_filename
+  ]
 }
 
-data "archive_file" "lambda_cloudfront_basic_auth_source_code"{
+data "archive_file" "lambda_cloudfront_basic_auth_source_code" {
   output_path = "/tmp/lambda_cloudfront_basic_auth_soruce_code.zip"
   type        = "zip"
   source {
     filename = "index.js"
     content  = <<-EOF
-      function handler(event) {
-        var errorResponse = {
-          status: 401,
-          statusDescription: "Unauthorized",
-          headers: {
-            "www-authenticate": {
-              value: 'Basic realm="Enter credentials for this super secure site"',
+      exports.handler = (event, context, callback) => {
+        var request = event.Records[0].cf.request;
+        var authHeaders = request.headers.authorization;
+        var expected = "Basic ZHluYW06JiFlJTNONWRkJHgza150OQ==";
+
+        if (authHeaders.value !== expected) {
+          var response = {
+            status: '200',
+            statusDescription: 'OK'
+            headers: {
+              'www-authenticate': [{
+                  key: 'WWW-Authenticate',
+                  value: 'Basic realm="Enter credentials for this super secure site"'
+              }],
             },
-          },
-        };
-
-        const request = event.Records[0].cf.request;
-
-        if (!request || !request.headers) {
-          return errorResponse;
+          };
+          callback(null, response);
         }
-
-        // Extract the authorization header
-        const authHeaders = request.headers['authorization'];
-
-        // Check the authorization header against the expected value
-        if (!authHeaders || !authHeaders[0] || authHeaders[0].value !== "Basic ZHluYW06JiFlJTNONWRkJHgza150OQ==") {
-            return errorResponse;
-        }
-
-        return request;
-      }
-
-      module.exports.handler = handler;
+      };
     EOF
   }
 }
