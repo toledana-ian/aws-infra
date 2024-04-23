a81b5ebc3dd73a1b7e3d1ec576db3eee57bc998e
Christian Toledana <toledana.ian@gmail.com>
2024-04-24 00:57:38 +0800

    Add digest authentication to CloudFront configuration
    Circle back on "app-email-blast-composer" CloudFront configuration to enable the use of digest authentication. Made possible by a parameter addition 'enable_digest_authentication', this commit introduces an added layer of security to the system catered to unique user requirements.

--- modules/app-email-blast-composer/cloudfront.tf
+++ modules/app-email-blast-composer/cloudfront.tf
@@ -74,6 +74,14 @@ resource "aws_cloudfront_distribution" "app" {
       }
     }
 
+    dynamic "function_association" {
+      for_each = var.enable_digest_authentication ? [1] : []
+      content {
+        event_type   = "viewer-request"
+        function_arn = aws_cloudfront_function.digest_authentication[0].arn
+      }
+    }
+
     viewer_protocol_policy = "redirect-to-https"
   }
 
