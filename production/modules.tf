80f0cae1a177013b2ba0513d50d3daddc658122b
Christian Toledana <toledana.ian@gmail.com>
2024-05-17 19:45:42 +0800

    Add file metadata viewer module in production environment
    This commit includes the addition of the "app-file-metadata-viewer" module in the production configuration. It is configured to use digest authentication and tagged as part of the "file-metadata-viewer" project. The provider "registry.terraform.io/hashicorp/archive" is removed from the .terraform.lock.hcl.

--- production/modules.tf
+++ production/modules.tf
@@ -14,3 +14,20 @@ module "app-email-blast-composer" {
     Project = "email-blast-composer"
   })
 }
+
+module "app-file-metadata-viewer" {
+  source = "../modules/app-static-site-host"
+  name   = "prod-file-metadata-viewer"
+
+  enable_digest_authentication = true
+
+  route_app_sub_domain_name = "dynamdev-file-metadata-viewer"
+  route_domain_name         = data.terraform_remote_state.global.outputs.route53_zone_name_christiantoledana_com
+  route_zone_id             = data.terraform_remote_state.global.outputs.route53_zone_id_christiantoledana_com
+
+  acm_certificate_arn = data.terraform_remote_state.global.outputs.acm_certificate_arn_christiantoledana_com
+
+  tags = merge(local.default_tags, {
+    Project = "file-metadata-viewer"
+  })
+}
