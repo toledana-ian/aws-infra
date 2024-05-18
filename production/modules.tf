aa89207d7ae8e58fcef863cabc97ab02281e8720
Christian Toledana <toledana.ian@gmail.com>
2024-05-18 12:50:27 +0800

    Disable digest authentication for prod-file-metadata-viewer
    Digest authentication for the "prod-file-metadata-viewer" module in the Terraform files has been disabled. This change was made in the "production/modules.tf" file. This can affect how clients authenticate to the server, so additional checks or updates may be required.

--- production/modules.tf
+++ production/modules.tf
@@ -19,7 +19,7 @@ module "app-file-metadata-viewer" {
   source = "../modules/app-static-site-host"
   name   = "prod-file-metadata-viewer"
 
-  enable_digest_authentication = true
+  enable_digest_authentication = false
 
   route_app_sub_domain_name = "dynamdev-file-metadata-viewer"
   route_domain_name         = data.terraform_remote_state.global.outputs.route53_zone_name_christiantoledana_com
