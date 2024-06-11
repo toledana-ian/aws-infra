2659e7d19049490d081fe313f955fdcbffd9b870
Christian Toledana <toledana.ian@gmail.com>
2024-06-11 22:27:28 +0800

    chore: Remove 'app-email-blast-composer' module and associated providers
    - Removed the entire 'app-email-blast-composer' module from production/modules.tf
    - Removed the 'registry.terraform.io/hashicorp/null', and 'registry.terraform.io/hashicorp/random' providers from the .terraform.lock.hcl file, as they were no longer needed after removing the module

--- production/modules.tf
+++ production/modules.tf
@@ -1,21 +1,3 @@
-module "app-email-blast-composer" {
-  source = "../modules/app-email-blast-composer"
-  name   = "prod-email-blast-composer"
-  random_suffix = "061124"
-
-  enable_digest_authentication = true
-
-  route_app_sub_domain_name = "dynamdev-email-blast-composer"
-  route_domain_name         = data.terraform_remote_state.global.outputs.route53_zone_name_christiantoledana_com
-  route_zone_id             = data.terraform_remote_state.global.outputs.route53_zone_id_christiantoledana_com
-
-  acm_certificate_arn = data.terraform_remote_state.global.outputs.acm_certificate_arn_christiantoledana_com
-
-  tags = merge(local.default_tags, {
-    Project = "email-blast-composer"
-  })
-}
-
 module "app-file-metadata-viewer" {
   source = "../modules/app-static-site-host"
   name   = "prod-file-metadata-viewer"
