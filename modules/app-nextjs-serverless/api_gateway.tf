532f6dd8def24e9c237148eefbda5c01ac9661b4
Christian Toledana <toledana.ian@gmail.com>
2024-06-14 18:53:56 +0800

    chore: Remove API root resource from API gateway
    - Removed the API root resource block in `api_gateway.tf` to simplify the configuration
    - The parent_id for the lambda_rest resource now uses the root_resource_id of the API directly instead of going through a now-deleted intermediate API resource

--- modules/app-nextjs-serverless/api_gateway.tf
+++ modules/app-nextjs-serverless/api_gateway.tf
@@ -40,23 +40,13 @@ resource "aws_api_gateway_stage" "api" {
   }
 }
 
-//########## API ROOT RESOURCE ##########
-
-resource "aws_api_gateway_resource" "api" {
-  count = var.is_lamba_zip_uploaded ? 1 : 0
-
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
-  path_part   = "api"
-}
-
 //########## LAMBDA REST RESOURCE ##########
 
 resource "aws_api_gateway_resource" "lambda_rest" {
   count = var.is_lamba_zip_uploaded ? 1 : 0
 
   rest_api_id = aws_api_gateway_rest_api.api.id
-  parent_id   = aws_api_gateway_resource.api[1].id
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
   path_part   = "*"
 }
 
