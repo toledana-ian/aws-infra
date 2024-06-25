54b488d52f5e7ffb5909080997ad658199577a9a
Christian Toledana <toledana.ian@gmail.com>
2024-06-25 10:38:41 +0800

    feat: Update condition for aws_cloudfront_key_value_store_credentials_store_id in local.tf of app-breadly module
    - Previously, aws_cloudfront_key_value_store_credentials_store_id was only generated when var.enable_digest_authentication was set to true.
    - This revision now ensures that aws_cloudfront_key_value_store_credentials_store_id is also generated when var.enable_digest_authentication_frontend or var.enable_digest_authentication_mint_tracker is set to true. This change will provide a more flexible mechanism to control digest authentication.

--- modules/app-breadly/local.tf
+++ modules/app-breadly/local.tf
@@ -3,5 +3,5 @@ locals {
   s3_bucket_name = "${var.name}-${var.random_suffix}"
 
   // Get the  last segment of the ARN of the credentials_store of aws_cloudfront_key_value_store
-  aws_cloudfront_key_value_store_credentials_store_id = var.enable_digest_authentication ? element(split("/", aws_cloudfront_key_value_store.credentials_store[0].arn), length(split("/", aws_cloudfront_key_value_store.credentials_store[0].arn)) - 1) : ""
+  aws_cloudfront_key_value_store_credentials_store_id = var.enable_digest_authentication_frontend||var.enable_digest_authentication_mint_tracker ? element(split("/", aws_cloudfront_key_value_store.credentials_store[0].arn), length(split("/", aws_cloudfront_key_value_store.credentials_store[0].arn)) - 1) : ""
 }
